155e1a4805c8c2ad9ca19bbeb2f0f12aa7567910
Stan-breaks <stanleymwendwa03@gmail.com>
2023-12-15 16:33:38 +0300

    day3 progress

--- day3/python/part1/script.py
+++ day3/python/part1/script.py
@@ -2,9 +2,29 @@ import sys
 
 
 def readFile(file):
-    with open(file, 'r') as f:
-        for line in f:
+    cs = set()
+    with open(file, "r") as f:
+        data = f.read()
+        lines = data.strip().split("\n")
+        for r in range(len(lines)):
+            for c in range(len(lines[r])):
+                if lines[r][c].isdigit() or lines[r][c] == ".":
+                    continue
+                for cr in [r - 1, r, r + 1]:
+                    for cc in [c - 1, c, c + 1]:
+                        if (
+                            cr < 0
+                            or cr >= len(lines)
+                            or cc < 0
+                            or cc >= len(lines[cr])
+                            or not lines[cr][cc].isdigit()
+                        ):
+                            continue
+                        while cc > 0 and lines[cr][cc - 1].isdigit():
+                            cc -= 1
+                        cs.add((cr, cc))
+    print(cs)
 
 
 if __name__ == "__main__":
-    answer = readFile(sys.argv[1]) 
+    readFile(sys.argv[1])
